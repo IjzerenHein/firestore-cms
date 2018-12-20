d53101ac149cfb4de4cecb9d1418aaf45be4ddaf
Hein Rutjes <hrutjes@gmail.com>
2018-12-20 19:53:42 +0100

    Added init dir

---
+++ init/config.js
@@ -0,0 +1,15 @@
+const collections = {};
+
+window.firestoreCMSConfig = {
+  title: "Firestore CMS",
+  firebase: {
+    apiKey: "XXXXXXXXXXXXXxxxxXXXXxxxXXXXXXXxxxXX",
+    authDomain: "xxxxxxxxxxxxxx.firebaseapp.com",
+    databaseURL: "https://xxxxxxxxxxxxxx.firebaseio.com",
+    projectId: "xxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxx.appspot.com",
+    messagingSenderId: "XXXXXXXXXX"
+  },
+  isPersistent: true,
+  collections: collections
+};
